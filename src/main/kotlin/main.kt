d7d1eb18e77e91b082b46e80a6b79c0588665919
AlekseyBelyaev2606 <partchoice44@gmail.com>
2022-08-01 19:13:29 +0300

    first change: repair camelCase, add data classes, change fun add and upgrate

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,12 +9,12 @@ data class Post(
     val text: String,
     val replyPostId: Int,
     val friendsOnly: Boolean,
-    val comments: Objects,
-    val copyright: Objects,
-    val likes: Objects,
-    val repost: Objects,
-    val views: Objects,
-    val post_type: String,
+    val comments: Comments,
+    val copyright: Copyright,
+    val likes: Likes,
+    val repost: Repost,
+    val views: Views,
+    val postType: String,
     val signerId: Boolean,
     val canPin: Boolean,
     val canDelete: Boolean,
@@ -23,27 +23,53 @@ data class Post(
     val markedAsAds: Boolean,
     val isFavorite: Boolean,
     val postponedId: Boolean,
-)
+){
+    data class Views(
+        val count: Int = 0
+    )
+    data class Repost(
+        val count: Int = 0,
+        val userReposted: Boolean = false
+    )
+
+    data class Comments(
+        val count: Int = 0,
+        val canPost: Boolean = false,
+        val groupsCanPost: Boolean = false,
+        val canClose: Boolean = false,
+        val canOpen: Boolean = false
+    )
+
+    data class Copyright(
+        val id: Int = 0,
+        val link: String,
+        val name: String,
+        val type: String
+    )
+
+    data class Likes(
+        val count: Int = 0,
+        val userLikes: Boolean = false,
+        val canLike: Boolean = false,
+        val canPublish: Boolean = false,
+    )
+
+}
 
 fun main() {
 }
 
 object WallService {
     private var posts = emptyArray<Post>()
+    private var id = 0
 
-    fun add(post: Post, id: Int): Post {
-        posts += post
-        for ((index, post) in posts.withIndex())
-            if (post.id == id) {
-                posts[index] = post.copy(id = post.id + 1)
-            }
+    fun add(post: Post): Post {
+        posts += post.copy(id = ++id)
         return posts.last()
-
     }
 
-    fun update(post: Post, id: Int): Boolean {
-        for ((index, post) in posts.withIndex())
-            if (post.id != id) {
+    fun update(post: Post): Boolean {
+        if (post.id != id) {
                 return true
             }
         return false
---
+++ src/test/kotlin/WallServiceTest.kt
@@ -0,0 +1,10 @@
+import org.junit.Assert.*
+
+class WallServiceTest {
+
+    @org.junit.Test
+    fun add(Post: Any?) {
+        val result = add(Post)
+        assertEquals(0, result)
+    }
+}
